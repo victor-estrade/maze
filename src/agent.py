e1f9297f227fae640c1db50662f1c6993f7efdf7
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-05 15:42:51 +0200

    add PolicyDriveAgent

--- src/agent.py
+++ src/agent.py
@@ -8,3 +8,12 @@ class RandomAgent():
     def play(self, observation, reward):
         action = np.random.choice(list(Action))
         return action
+
+
+class PolicyDrivenAgent():
+    def __init__(self, policy):
+        self.policy = policy
+
+    def play(self, observation, reward):
+        move = self.policy.next_move(observation)
+        return move
