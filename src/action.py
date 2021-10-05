a38c5c888224eadd9397d55c7f963aafc35b9f83
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-05 14:51:32 +0200

    Add next_position() to Action

--- src/action.py
+++ src/action.py
@@ -6,3 +6,6 @@ class Action(Enum):
     DOWN = (0, 1)
     LEFT = (-1, 0)
     RIGHT = (1, 0)
+
+    def next_position(self, position):
+        return (self.value[0] + position[0], self.value[1] + position[1])
