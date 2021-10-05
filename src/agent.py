f10906d57298a0c8efd4beb7cf267b75c031f1c0
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-05 12:55:41 +0200

    Add random agent

--- src/agent.py
+++ src/agent.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+import numpy as np
+
+from src.action import Action
+
+class RandomAgent():
+    def play(self, observation, reward):
+        action = np.random.choice(list(Action))
+        return action
