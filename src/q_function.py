eb91b541734cfcc1c0cc75c4cfaa9f2a0b860c8d
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-10-05 15:47:58 +0200

    Init q_functions

---
+++ src/q_function.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from src.action import Action
+
+class Qfunction():
+    def __init__(self, world, policy, gamma=1):
+        self.world = world
+        self.policy = policy
+        self.gamma = gamma
+
+    def q_value(self, action, state):
+        old_cumulative_reward = self.world.cumulative_reward
+        old_state = self.world.state
+
+        self.world.state = state
+        observation = state
+        done = False
+        move = action
+        all_rewards = []
+        while not done:
+            observation, reward, done = self.world.step(move, observation)
+            move = self.policy.next_move(observation)
+            all_rewards.append(reward)
+        value = self.gamma_cumulative(all_rewards)
+
+        self.world.cumulative_reward = old_cumulative_reward
+        self.world.state = old_state
+        return value
+
+    def __call__(self, action, state):
+        return self.q_value(action, state)
+
+    def gamma_cumulative(self, all_rewards):
+        cumul = 0
+        for reward in all_rewards[::-1]:
+            cumul = self.gamma * cumul + reward
+        return cumul
+
+    def compute_all_q_value(self):
+        q_value_table = {}
+        for state in self.world.all_possible_states():
+            for action in list(Action):
+                q_value_table[(state, action)] = self.q_value(action, state)
+        return q_value_table
